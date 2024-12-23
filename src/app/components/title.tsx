366424251ee2ad3dbe38956a704787ffdad63358
ry_Tom <ryotoyota@outlook.jp>
2024-12-23 14:46:25 +0900

    decrease title margin top

--- src/app/components/title.tsx
+++ src/app/components/title.tsx
@@ -2,7 +2,7 @@ import { TITLE } from "@/lib/constants"
 
 const Title = () => {
   return (
-    <header className="mt-20 mb-10">
+    <header className="mt-14 mb-10">
       <h1 className="text-3xl font-bold flex justify-center">{TITLE}</h1>
     </header>
   )
